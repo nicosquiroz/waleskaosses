26adfc277b824cdb2650b2b3136a756317b4a0be
Nicolás S. Quiroz <nicosquiroz@gmail.com>
2023-10-12 02:45:01 -0300

    Routing

---
+++ src/Landing.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Negocios.css";
+
+export const Landing = () => {
+  return (
+    <div>Waleska Osses</div>
+
+
+
+
+
+
+
+
+
+
+    
+  );
+
+};
+
+
+export default Landing;
---
+++ src/Negocios.js
@@ -0,0 +1,88 @@
+import React from "react";
+import "./Negocios.css";
+
+export const Negocios = () => {
+  return (
+    <div className="macbook-air">
+      <p className="vi-a-del-mar">
+        Viña del Mar - Santiago CL
+        <br />
+        Física y Economía
+      </p>
+      <div className="overlap">
+        <p className="enfocando">
+          Enfocando herramientas de la ciencia, creatividad y tecnología a resolver problemas <br />
+          en el mundo del emprendimiento.
+        </p>
+        <div className="frame">
+          <div className="rectangle" />
+        </div>
+        <div className="rectangle-wrapper">
+          <div className="div" />
+        </div>
+      </div>
+      <img className="line" alt="Line" src="line-1.svg" />
+      <div className="overlap-group">
+        <div className="code">&lt;qué hacemos&gt;</div>
+        <div className="text-wrapper">are</div>
+      </div>
+      <div className="overlap-2">
+        <div className="code">&lt;nuestros servicios&gt;</div>
+        <div className="HERRAMIENTAS-ESPEC">
+          HERRAMIENTAS
+          <br />
+          ESPECÍFICAS
+        </div>
+      </div>
+      <div className="code">&lt;contáctanos&gt;</div>
+      <div className="overlap-3">
+        <div className="text-wrapper-2">(Thats here) That’s us.</div>
+        <div className="group">
+          <div className="image-wrapper">
+            <img className="image" alt="fotos" src=".img/Group 9.png" />
+          </div>
+        </div>
+        <div className="text-wrapper-3">That’s here. That’s us.</div>
+        <p className="interested-in-the">Interested in the project? Contact us here.</p>
+      </div>
+      <div className="modelo-de-negocio">
+        Modelo de negocio,
+        <br />
+        Branding,
+        <br />
+        Web
+      </div>
+      <p className="p">
+        _Programa de sesiones para llevar a tu negocio a consolidar su idea temprana, hacer pruebas de validación,
+        fundamentación de marca y la creación del MVP.
+      </p>
+      <p className="text-wrapper-4">
+        _Trabajamos con emprendedores del área STEM ayudándolos a desarrollar su marca, modelo de negocio y su visión
+        científica única.
+      </p>
+      <p className="text-wrapper-5">
+        _Ocupamos estrategias, y conocimientos especializados científicos que podemos entender y compartir para entregar
+        soluciones de diseño de marca y de productos.
+      </p>
+      <div className="overlap-4">
+        <div className="MODELO-DE-NEGOCIO">
+          _MODELO DE NEGOCIO
+          <br />
+          _IDENTIDAD VISUAL
+          <br />
+          _ASESORÍA&nbsp;&nbsp;DE&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;PITCH
+        </div>
+        <div className="REBRANDING-ASESOR-a">
+          _REBRANDING
+          <br />
+          _ASESORÍA DE LINKEDIN
+          <br />
+          _DISEÑO WEB
+        </div>
+      </div>
+      <p className="want-to-work">Want to work together? -&gt; Here (asesoría)</p>
+    </div>
+  );
+};
+
+export default Negocios;
