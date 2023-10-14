0cd19a51245f084604da4181d59e330af5b38faa
Nicolás S. Quiroz <nicosquiroz@gmail.com>
2023-10-14 15:20:54 -0300

    sección 6 fondo

--- src/Landing.js
+++ src/Landing.js
@@ -7,6 +7,7 @@ import Seccion2 from "./sections/2";
 import Seccion3 from "./sections/3";
 import Seccion4 from "./sections/4";
 import Seccion5 from "./sections/5";
+import Seccion6 from "./sections/6";
 export const Landing = () => {
   return (
     <div className="web">
@@ -15,6 +16,7 @@ export const Landing = () => {
       <Seccion3/>
       <Seccion4/>
       <Seccion5/>
+      <Seccion6/>
       
       {/* https://codesandbox.io/s/desktop-momentum-scrollingparallax-react-t8wr5?file=/src/App.js */}
     {/* <img className="web" src={web}/> */}
---
+++ src/sections/6.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./6.css";
+import sobremí from "../img/sobremí.svg";  
+import separador2 from "../img/separador2.svg";
+
+
+
+export const Seccion6 = () => {
+return (
+
+    <div className="sec6">
+        
+<div className="frame">
+
+<div className="fondonegro">
+<img className="separador2" src={separador2}/>
+<img className="sobremí" src={sobremí}/>
+</div>
+
+</div>
+</div>
+);
+};
+export default Seccion6;
