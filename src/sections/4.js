0cbe6612de1479c6872fde7f4aa0fe3abc242685
Nicolás S. Quiroz <nicosquiroz@gmail.com>
2023-10-15 17:25:32 -0300

    testimonios css

--- src/sections/4.js
+++ src/sections/4.js
@@ -16,30 +16,31 @@ return (
         
     <div className="frame">
     <div className="exito">TESTIMONIOS</div>
-    
+    <div class="grid-container">
     <div className="testimonios">
-    <p>No solo fue solo de trabajo, sino también de autoestima, fue muy
+    <p class="grid-item">No solo fue solo de trabajo, sino también de autoestima, fue muy
 hermoso.</p>
 
-<p>Lo que más rescato de Waleska es que te motiva a más no poder, y te
+<p class="grid-item">Lo que más rescato de Waleska es que te motiva a más no poder, y te
 hace confiar más en uno mismo, cosas que son totalmente necesarias a la
 hr de comenzar a postular a trabajos</p>
 
-<p>Bueno, mi experiencia con Waleska literal me cambió la vida jajaj ya
+<p class="grid-item">Bueno, mi experiencia con Waleska literal me cambió la vida jajaj ya
 que en el último tiempo estuve tan triste y frustrada con mi trabajo</p>
 
-<p>En las entrevistas me preguntaron exactamente lo que Waleska me dijo
+<p class="grid-item">En las entrevistas me preguntaron exactamente lo que Waleska me dijo
 que iban a preguntar.</p>
 
-<p>Desde que me hablaron por primera vez hasta la carta de oferta habrá
+<p class="grid-item">Desde que me hablaron por primera vez hasta la carta de oferta habrá
 pasado una semana?</p>
 
-<p>En menos de 2 semanas me llamaron que había sido seleccionado en la
-entrevista final</p>
+{/* <p class="grid-item">En menos de 2 semanas me llamaron que había sido seleccionado en la
+entrevista final</p> */}
 
-<p>Después de mi cambio de trabajo tripliqué mi sueldo</p>
+<p class="grid-item">Después de mi cambio de trabajo tripliqué mi sueldo</p>
 </div>
-<img className="tarjeta1" src={tarjeta1}/>
+</div>
+{/* <img className="tarjeta1" src={tarjeta1}/> */}
       </div>
     </div>
 );
