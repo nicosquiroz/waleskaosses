193bf7bcd12869e396d722ca1d8be48d3c4aa956
Nicolás S. Quiroz <nicosquiroz@gmail.com>
2023-10-16 19:13:06 -0300

    a

--- src/Landing.js
+++ src/Landing.js
@@ -15,17 +15,17 @@ export const Landing = () => {
   return (
     <div className="web">
       {/* <Negocios/> */}
-      {/* <Seccion1/>
+      <Seccion1/>
       <Seccion2/>
       <Seccion3/>
       <Seccion4/>
       <Seccion5/>
-      <Seccion6/> */}
+      <Seccion6/>
       {/* <Seccion7/> */}
       
       {/* https://codesandbox.io/s/desktop-momentum-scrollingparallax-react-t8wr5?file=/src/App.js */}
     {/* <img className="web" src={web}/> */}
-    <img className="webnegocios" src={negocios}/>
+    {/* <img className="webnegocios" src={negocios}/> */}
 
 
     
